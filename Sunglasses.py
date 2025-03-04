2b3c219d281c2d15c4af8e71c91195358cb6009b
DianaLee1219 <ldh05930@gmail.com>
2025-03-04 13:27:06 -0700

    Create Sunglasses.py

---
+++ Sunglasses.py
@@ -0,0 +1,81 @@
+# Import libraries
+import cv2
+import numpy as np
+from dataPath import DATA_PATH
+import os
+import matplotlib.pyplot as plt
+%matplotlib inline
+import matplotlib
+matplotlib.rcParams['figure.figsize'] = (6.0, 6.0)
+matplotlib.rcParams['image.cmap'] = 'gray'
+
+# Load the Face Image
+faceImagePath = os.path.join(DATA_PATH,"images/musk.jpg")
+faceImage = cv2.imread(faceImagePath)
+
+plt.imshow(faceImage[:,:,::-1]);plt.title("Face")
+
+# Load the Sunglass image with Alpha channel
+# (http://pluspng.com/sunglass-png-1104.html)
+glassimagePath = os.path.join(DATA_PATH,"images/sunglass.png")
+glassPNG = cv2.imread(glassimagePath,-1)
+
+# Resize the image to fit over the eye region
+glassPNG = cv2.resize(glassPNG,(300,100))
+print("image Dimension ={}".format(glassPNG.shape))
+
+# Separate the Color and alpha channels
+glassBGR = glassPNG[:,:,0:3]
+glassMask1 = glassPNG[:,:,3]
+
+# Display the images for clarity
+plt.figure(figsize=[15,15])
+plt.subplot(121);plt.imshow(glassBGR[:,:,::-1]);plt.title('Sunglass Color channels');
+plt.subplot(122);plt.imshow(glassMask1,cmap='gray');plt.title('Sunglass Alpha channel');
+
+# Make a copy
+faceWithGlassesNaive = faceImage.copy()
+
+# Replace the eye region with the sunglass image
+faceWithGlassesNaive[150:250,140:440]=glassBGR
+
+plt.imshow(faceWithGlassesNaive[...,::-1])
+
+# Make the dimensions of the mask same as the input image.
+# Since Face Image is a 3-channel image, we create a 3 channel image for the mask
+
+glassMask = cv2.merge((glassMask1,glassMask1,glassMask1))
+
+# Make the values [0,1] since we are using arithmetic operations
+glassMask = np.uint8(glassMask/255)
+
+# Make a copy
+faceWithGlassesArithmetic = faceImage.copy()
+
+# Get the eye region from the face image
+eyeROI= faceWithGlassesArithmetic[150:250,140:440]
+
+# Use the mask to create the masked eye region
+maskedEye = cv2.multiply(eyeROI,(1-glassMask))
+
+# Use the mask to create the masked sunglass region
+maskedGlass = cv2.multiply(glassBGR,glassMask)
+
+# Combine the Sunglass in the Eye Region to get the augmented image
+eyeRoiFinal = cv2.add(maskedEye, maskedGlass)
+alpha = 0.7  # Transparency factor (0 = fully transparent, 1 = fully opaque)
+eyeRoiFinal = cv2.addWeighted(eyeROI, 1 - alpha, eyeRoiFinal, alpha, 0)
+
+# Display the intermediate results
+plt.figure(figsize=[20,20])
+plt.subplot(131);plt.imshow(maskedEye[...,::-1]);plt.title("Masked Eye Region")
+plt.subplot(132); plt.imshow(glassBGR[...,::-1]); plt.title("Sunglasses")
+plt.subplot(133); plt.imshow(eyeRoiFinal[...,::-1]); plt.title("Partially Transparent Sunglasses")
+
+# Replace the eye ROI with the output from the previous section
+faceWithGlassesArithmetic[150:250,140:440]=eyeRoiFinal
+
+# Display the final result
+plt.figure(figsize=[20,20]);
+plt.subplot(121);plt.imshow(faceImage[:,:,::-1]); plt.title("Original Image");
+plt.subplot(122);plt.imshow(faceWithGlassesArithmetic[:,:,::-1]);plt.title("With Sunglasses");
